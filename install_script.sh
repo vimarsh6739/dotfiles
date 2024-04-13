3a26a55f829289a0479be172f5db9f90be35c760
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2024-04-12 19:26:41 -0500

    Added curl based omz install

---
+++ install_script.sh
@@ -0,0 +1,11 @@
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv install.sh "$HOME"
+sh "$HOME/install.sh"
+
+# Install neovim locally
+
+
+# Set neovim config to my kickstarter with clangd
+
+
