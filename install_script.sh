fe314decebc0d2179a2e5058e237037592f94b64
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2024-04-12 19:48:54 -0500

    Added neovim install(local)

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,11 @@ mv install.sh "$HOME"
 sh "$HOME/install.sh"
 
 # Install neovim locally
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar -zxvf "$HOME/nvim-local" nvim-linux64.tar.gz
 
+# Add neovim PATH to zshrc
+echo 'export PATH=${HOME}/nvim-local:${PATH}' >> "$HOME/.zshrc"
 
 # Set neovim config to my kickstarter with clangd
 
