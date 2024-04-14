1e8c2de3cc535ef91faa594769ba70c2b82825fb
Vimarsh Sathia <vsathia2@illinois.edu>
2024-04-14 12:52:24 -0500

    zsh plugins, proper nvim install with aliasing

--- install_script.sh
+++ install_script.sh
@@ -3,13 +3,33 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 mv install.sh "$HOME"
 sh "$HOME/install.sh"
 
+# Configure extensions for oh-my-zsh
+# autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# update the plugins line in zshrc
+sed -i "s/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" "$HOME/.zshrc"
+
 # Install neovim locally
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar -zxvf "$HOME/nvim-local" nvim-linux64.tar.gz
+tar -zxvf nvim-linux64.tar.gz
 
-# Add neovim PATH to zshrc
-echo 'export PATH=${HOME}/nvim-local:${PATH}' >> "$HOME/.zshrc"
+mv nvim-linux64 "$HOME/nvim-local" 
+
+# Add neovim PATH to zshrc and config alias
+echo 'export PATH=${HOME}/nvim-local/bin:${PATH}' >> "$HOME/.zshrc"
+echo "alias vim='nvim'" >> "$HOME/.zshrc"
+
+# Delete zipped install for neovim
+echo 'Deleting zipped install for nvim'
+rm nvim-linux64.tar.gz
 
 # Set neovim config to my kickstarter with clangd
+mkdir -p "$HOME/.config"
+mv kickstart.nvim "$HOME/.config/nvim/"
 
-
+# Assume CUDA and CUDNN is already installed in $HOME/local
+# Add aliases to local path for the nvcc compiler
