799e981ba622a1dd4f1b87e674ce251494fd17f0
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2026-08-12 00:39:09 -0400

    add emacs-mac config

---
+++ .emacs.d/init.el
@@ -0,0 +1,6 @@
+;; Use left Option as Meta, while keeping right Option available for
+;; normal macOS character entry (for example, accented characters).
+(setq mac-option-modifier 'meta
+      mac-right-option-modifier nil)
+
+(setq mac-command-modifier 'super)
